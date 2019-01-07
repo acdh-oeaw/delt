c31e9562511a45b0ec30f277107a9a207c685743
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-01-07 09:12:08 +0100

    more info props

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -6,9 +6,9 @@ PROJECT_METADATA = {
     'author': 'Peter Andorfer',
     'subtitle': 'Database of English Learner Texts (DELT)',
     'description': """The Database of English Learner Texts (DELT) aims to promote research on the learning and use of English. The database is freely available to all members of the research community, provided they adhere to the conditions of use (see below).
-The database consists of a collection of texts (both written and oral) produced by learners of English. Background information on the texts and authors allows users of DELT to find texts that share certain characteristics, such as language proficiency level, text type/genre, modality etc. The resulting text corpora can then be analysed with tools such as AntWord ® (http://www.laurenceanthony.net/software/antwordprofiler/), WordSmith ® (https://www.lexically.net/wordsmith/) or others.
+    The database consists of a collection of texts (both written and oral) produced by learners of English. Background information on the texts and authors allows users of DELT to find texts that share certain characteristics, such as language proficiency level, text type/genre, modality etc. The resulting text corpora can then be analysed with tools such as AntWord ® (http://www.laurenceanthony.net/software/antwordprofiler/), WordSmith ® (https://www.lexically.net/wordsmith/) or others.
 
-The majority of participants have German as their dominant language and produced these texts in school or university settings. More detailed information on the texts and participants is provided in each text header."""
+    The majority of participants have German as their dominant language and produced these texts in school or university settings. More detailed information on the texts and participants is provided in each text header."""
 ,
     'github': 'https://github.com/acdh-oeaw/delt',
     'purpose_de': 'Ziel von DELT ist die Publikation von Forschungsdaten.',
@@ -17,4 +17,5 @@ The majority of participants have German as their dominant language and produced
     'matomo_id': '84',
     'matomo_url': '//matomo.acdh.oeaw.ac.at/',
     'imprint': '/imprint/',
+    'app_type': 'database',  # database|website|service|tool|digital-edition
 }
